d069375e602787d3ca84331ab925294ff0a60228
orbitrymusic <orbitrymusic@gmail.com>
2026-05-24 14:53:48 -0300

    lienzo en blanco

--- constants/theme.ts
+++ constants/theme.ts
@@ -5,13 +5,13 @@
 
 import { Platform } from 'react-native';
 
-const tintColorLight = '#0a7ea4';
+const tintColorLight = '#8a1e8cff';
 const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
     text: '#11181C',
-    background: '#fff',
+    background: '#d18deaff',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
